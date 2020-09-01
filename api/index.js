6edd9601c18237a6e5df93274e5aa1dd34a9b9ae
Dafiul Haq <dafiulhaq97@gmail.com>
2020-09-01 08:02:22 +0700

    self deploy top langs

---
+++ api/index.js
@@ -0,0 +1,64 @@
+require("dotenv").config();
+const {
+  renderError,
+  clampValue,
+  parseBoolean,
+  parseArray,
+  CONSTANTS,
+} = require("../src/utils");
+const fetchTopLanguages = require("../src/fetcher");
+const renderTopLanguages = require("../src/card");
+const blacklist = require("../src/blacklist");
+
+module.exports = async (req, res) => {
+  const username = "dafiulh";
+  const hide_border = true;
+  const {
+    //# username,
+    hide,
+    hide_title,
+    //# hide_border,
+    card_width,
+    title_color,
+    text_color,
+    bg_color,
+    theme,
+    cache_seconds,
+    layout,
+  } = req.query;
+  let topLangs;
+
+  res.setHeader("Content-Type", "image/svg+xml");
+
+  if (blacklist.includes(username)) {
+    return res.send(renderError("Something went wrong"));
+  }
+
+  try {
+    topLangs = await fetchTopLanguages(username);
+
+    const cacheSeconds = clampValue(
+      parseInt(cache_seconds || CONSTANTS.TWO_HOURS, 10),
+      CONSTANTS.TWO_HOURS,
+      CONSTANTS.ONE_DAY
+    );
+
+    res.setHeader("Cache-Control", `public, max-age=${cacheSeconds}`);
+    
+    return res.send(
+      renderTopLanguages(topLangs, {
+        hide_title: parseBoolean(hide_title),
+        hide_border: parseBoolean(hide_border),
+        card_width: parseInt(card_width, 10),
+        hide: parseArray(hide),
+        title_color,
+        text_color,
+        bg_color,
+        theme,
+        layout,
+      })
+    );
+  } catch (err) {
+    return res.send(renderError(err.message, err.secondaryMessage));
+  }
+};
